85fadafa1cff870de3105500b822d9c7e09cd785
Ivan <kvasovski34@gmail.com>
2021-10-11 15:02:47 +0300

    Done.

--- second_project/src/com/company/Main.java
+++ second_project/src/com/company/Main.java
@@ -3,8 +3,5 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         Mapp<Integer, Integer> map = new Mapp<>();
-        map.put(1, 2);
-        Object res = map.get(1);
-        System.out.println("res:" + res);
     }
 }
