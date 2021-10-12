07eeb77688a12821f09af3b53322d28a7a10e891
Ivan <kvasovski34@gmail.com>
2021-10-12 13:35:45 +0300

    bug fix

--- second_project/src/com/company/Main.java
+++ second_project/src/com/company/Main.java
@@ -6,15 +6,15 @@ public class Main {
         List_int list1 = new List_int();
         List_int list2 = new List_int();
         list1.insert(1);
+        list1.insert(3);
+        list1.insert(4);
         list1.insert(5);
-        list1.insert(10);
-        list1.insert(15);
-        list1.insert(21);
-        list2.insert(2);
-        list2.insert(13);
-        list2.insert(14);
-        list2.insert(17);
-        List_int list_merge = list1.merge(list2.head, list1.head) ;
+        list1.insert(6);
+        list2.insert(7);
+        list2.insert(8);
+        list2.insert(9);
+        list2.insert(10 );
+        List_int list_merge = list1.merge(list1.head, list2.head) ;
         list_merge.print();
     }
 }
