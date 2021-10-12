f523057cb33d1c42c78663e9dc5feee7234feac4
Ivan <kvasovski34@gmail.com>
2021-10-12 18:11:50 +0300

    Bag fix

--- second_project/src/com/company/Main.java
+++ second_project/src/com/company/Main.java
@@ -1,22 +1,19 @@
 package com.company;
 
-import jdk.swing.interop.SwingInterOpUtils;
-
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         List_int list1 = new List_int();
         List_int list2 = new List_int();
         list1.insert(1);
-        list1.insert(3);
-        list1.insert(4);
         list1.insert(5);
-        list1.insert(6);
-        list2.insert(7);
-        list2.insert(8);
-        list2.insert(9);
-        list2.insert(10);
+        list1.insert(10);
+        list1.insert(15);
+        list1.insert(20);
+        list2.insert(3);
+        list2.insert(6);
+        list2.insert(13);
+        list2.insert(14);
         List_int list_merge = list1.merge(list1.head, list2.head);
         list_merge.print();
 /*        Scanner scanner = new Scanner(System.in);
